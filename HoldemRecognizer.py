064bde3b9d81b485d4c93ea084eab0e929487c20
EdelEB <edeleb@comcast.net>
2021-11-22 17:18:50 -0500

    HandRecognizer works as intended

--- HoldemRecognizer.py
+++ HoldemRecognizer.py
@@ -31,7 +31,6 @@ def HandRecognizer(hole, board): # Card[] hole cards, Card[] community cards
             vals.append(card.val);
 
     vals = sorted(vals,key=None,reverse=True); # descending order
-    print(vals)
 
     if  x := check_sf(all_cards, vals):
         return x;
@@ -50,6 +49,7 @@ def HandRecognizer(hole, board): # Card[] hole cards, Card[] community cards
     elif check_pair(vals):
         return check_pair(vals);
     else:
+        vals = vals[0:len(vals)-3];
         vals.insert(0,1);
         return vals;
 
@@ -66,17 +66,17 @@ def check_sf(cards, vals):
 
 # check for four of a kind
 # returns [hand rank, val of card that there are four of] || False
-def check_four(arr):
-    for x in arr:
-        if subset([x,x,x,x], arr):
+def check_four(vals):
+    for x in vals:
+        if subset([x,x,x,x], vals):
             return [8, x];
     return False;
 
 # checks for full house
 # returns [hand rank, three card val, pair card val] || False
-def check_full(arr):
-    three = check_three(arr)
-    two_tup = check_two_pair(arr)
+def check_full(vals):
+    three = check_three(vals)
+    two_tup = check_two_pair(vals)
     if three and two_tup:
         two_tup.pop(two_tup.index(three[1]));
         return [7, three[1], two_tup[1]];
@@ -108,50 +108,52 @@ def check_flush(cards): # returns flush suit
         return ret;
     return False;
 
-def check_straight(arr):
+def check_straight(vals):
     for i in range(14, 4, -1):
         sub = [i,i-1,i-2,i-3,i-4];
-        if subset(sub, arr):
+        if subset(sub, vals):
             sub.insert(0,5);
             return sub;
 
 # check if three of a kind
-def check_three(arr):
-    for x in arr:
-        if subset([x,x,x], arr):
-            temp = [y for y in arr if y != x];
+def check_three(vals):
+    for x in vals:
+        if subset([x,x,x], vals):
+            temp = [y for y in vals if y != x];
             return [4, x, temp[0], temp[1]];
     return False;
 
-def check_two_pair(arr):
-    while 1 in arr: # removes extra Ace vals, 14 is in arr already
-        arr.pop(len(arr)-1);
+def check_two_pair(vals):
+    temp = [num for num in vals if num != 1]; # removes Ace 1's (14's are already in list)
 
+    pairs = [];
     ret = [];
-    for x in arr:
-        if len(ret)==2:
-            break;
-        if subset([x,x], arr):
-            ret.append(x);
-            arr = [y for y in arr if y != x];
 
-    if ret:
+    for x in temp:
+        if len(ret)>1:
+            break;
+        if x not in pairs and subset([x,x], temp):
+            ret.append(x);
+            pairs.append(x);
+
+    if len(ret) > 1:
         ret.insert(0,3);
-        ret.append(arr[0]);
+        ret.append(temp[0]);
         return ret;
     return False;
 
-def check_pair(arr):
-    print(arr);
-    for x in arr:
-        if subset([x,x], arr):
-            #arr.pop(arr.index(x));
-            #arr.pop(arr.index(x));
-            return [1, x, arr[0], arr[1], arr[2]];
+def check_pair(vals):
+    temp = vals.copy();
+    for x in temp:
+        if subset([x,x], temp):
+            temp = [y for y in temp if y != x];
+            # temp.pop(temp.index(x));
+            # temp.pop(temp.index(x));
+            return [2, x, temp[0], temp[1], temp[2]];
     return False;
 
 def subset(sub, full):
-    temp = full;
+    temp = full.copy();
     for x in sub:
         if x in temp:
             temp.pop(temp.index(x));
@@ -226,11 +228,11 @@ class DrawRecognizer(object):
 
 def test():
 
-    import BotHoldem;
+    from BotHoldem import BotHoldem as BotH;
     from Deck import Deck;
     d = Deck();
-    #bot = BotHoldem.BotHoldem(1, 200);
-    """
+    #bot = BotH(1, 200);
+
     for i in range(20):
         d.shuffle();
         com = [d.deal_one(), d.deal_one(), d.deal_one(), d.deal_one(), d.deal_one()];
@@ -239,12 +241,15 @@ def test():
             print(card, end=" ");
         print();
         print(HandRecognizer(hole, com));
-    """
-    arr = [1, 3, 2, 2, 3, 5, 6];
-    print(check_four(arr));
-    print(check_straight(arr))
-    print(check_two_pair(arr))
+        print();
 
+    # sub = [1, 5];
+    # arr = [1, 3, 2, 2, 3, 5, 6];
+    # print(arr)
+    # print(check_four(arr));
+    # print(check_straight(arr))
+    # print(check_two_pair(arr))
+    # print(arr);
     # board_pair:flush_draw:straight_draw
 test();
 
